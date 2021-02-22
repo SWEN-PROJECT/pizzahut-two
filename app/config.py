934fa0abf53c050705cdae50cbb1961836a18b15
Matthew Palmer <palmer.matthew4567@gmail.com>
2021-02-22 18:14:36 -0500

    Changes to structure

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Ay6df7*&$GHxt86!'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
